ab2e4784fbf8b369b8af872a7aea3cf959919470
Elliot J. Reed <git@elliotjreed.com>
2021-04-25 11:42:59 +0100

    Removes CORs from Symfony and adds to NginX

--- config/bundles.php
+++ config/bundles.php
@@ -7,6 +7,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true]
 ];
