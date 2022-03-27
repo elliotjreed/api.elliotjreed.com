d89f3db8715c0973e13f620cd063ca77245f441b
Elliot J. Reed <git@elliotjreed.com>
2022-03-27 23:18:13 +0100

    Refactors Email Address test

--- tests/Validator/EmailAddressTest.php
+++ tests/Validator/EmailAddressTest.php
@@ -7,6 +7,7 @@ namespace App\Tests\Validator;
 use App\Validator\EmailAddress;
 use ElliotJReed\DisposableEmail\Email;
 use PHPUnit\Framework\TestCase;
+use SplFileObject;
 
 final class EmailAddressTest extends TestCase
 {
@@ -18,9 +19,9 @@ final class EmailAddressTest extends TestCase
     public function testItReturnsFalseWithInvalidEmailAddressErrorMessageWhenEmailAddressIsInvalid(): void
     {
         $filePath = \sys_get_temp_dir() . '/phpunit-emailaddresstest-disposable-list.txt';
-        $disposableEmailAddressFile = new \SplFileObject($filePath, 'wb+');
+        $file = new SplFileObject($filePath, 'wb+');
 
-        $validator = new EmailAddress(new Email($disposableEmailAddressFile->getRealPath()));
+        $validator = new EmailAddress(new Email($file->getRealPath()));
 
         $this->assertFalse($validator->valid('Clearly this is not an email address'));
         $this->assertSame(['The email address appears to be invalid.'], $validator->getErrors());
@@ -29,10 +30,10 @@ final class EmailAddressTest extends TestCase
     public function testItReturnsFalseWithInvalidEmailAddressMessageWhenEmailAddressIsDisposableEmailAddress(): void
     {
         $filePath = \sys_get_temp_dir() . '/phpunit-emailaddresstest-disposable-list.txt';
-        $disposableEmailAddressFile = new \SplFileObject($filePath, 'wb+');
-        $disposableEmailAddressFile->fwrite('example.com');
+        $file = new SplFileObject($filePath, 'wb+');
+        $file->fwrite('example.com');
 
-        $validator = new EmailAddress(new Email($disposableEmailAddressFile->getRealPath()));
+        $validator = new EmailAddress(new Email($file->getRealPath()));
 
         $this->assertFalse($validator->valid('email@example.com'));
         $this->assertSame(['The email address appears to be invalid.'], $validator->getErrors());
@@ -41,10 +42,10 @@ final class EmailAddressTest extends TestCase
     public function testItReturnsTrueWhenEmailAddressAppearsToBeValid(): void
     {
         $filePath = \sys_get_temp_dir() . '/phpunit-emailaddresstest-disposable-list.txt';
-        $disposableEmailAddressFile = new \SplFileObject($filePath, 'wb+');
-        $disposableEmailAddressFile->fwrite('test.com');
+        $file = new SplFileObject($filePath, 'wb+');
+        $file->fwrite('test.com');
 
-        $validator = new EmailAddress(new Email($disposableEmailAddressFile->getRealPath()));
+        $validator = new EmailAddress(new Email($file->getRealPath()));
 
         $this->assertTrue($validator->valid('email@example.com'));
         $this->assertSame([], $validator->getErrors());
